078bf02f93f47f7b72639efbcd9c691ee7df4207
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-11 01:12:53 +0200

    testComplexScoreCombination()test

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ScoreKeeperTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ScoreKeeperTest.java
@@ -80,7 +80,7 @@ public void testScoreTeamB1() {
         scoreKeeper.scoreTeamB3();
         scoreKeeper.scoreTeamA1();
         scoreKeeper.scoreTeamB1();
-        Assertions.assertEquals("006:006x", scoreKeeper.getScore());
+        Assertions.assertEquals("006:006", scoreKeeper.getScore());
     }
  /* 
     @Test
