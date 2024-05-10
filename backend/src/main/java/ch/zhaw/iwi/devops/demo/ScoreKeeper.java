6b434cb2282f940b5a0203831e76ead234b15eff
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-11 00:28:53 +0200

    testScoreTeamA3()test

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/ScoreKeeper.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/ScoreKeeper.java
@@ -3,11 +3,11 @@ package ch.zhaw.iwi.devops.demo;
 public class ScoreKeeper {
     private int scoreTeamA = 0;
     private int scoreTeamB = 0;
-/* 
+ 
     public void scoreTeamA3() {
         scoreTeamA += 3;
     }
-
+/* 
     public void scoreTeamA2() {
         scoreTeamA += 2;
     }
@@ -27,9 +27,8 @@ public class ScoreKeeper {
     public void scoreTeamB1() {
         scoreTeamB += 1;
     }
-
+*/
     public String getScore() {
         return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
     }
 }
-*/
