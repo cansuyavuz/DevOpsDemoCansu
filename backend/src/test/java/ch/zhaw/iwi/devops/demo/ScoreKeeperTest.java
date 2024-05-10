1ab1c5cc0d720352dbc7ea4912ec58fffd8a1531
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-11 00:43:19 +0200

    testScoreTeamA1()fehler

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ScoreKeeperTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ScoreKeeperTest.java
@@ -17,20 +17,20 @@ public class ScoreKeeperTest {
         scoreKeeper.scoreTeamA3();
         Assertions.assertEquals("003:000", scoreKeeper.getScore());
     }
-    
+
     @Test
     public void testScoreTeamA2() {
         scoreKeeper.scoreTeamA2();
         Assertions.assertEquals("002:000", scoreKeeper.getScore());
     }
 
-/* 
+
     @Test
     public void testScoreTeamA1() {
         scoreKeeper.scoreTeamA1();
         Assertions.assertEquals("001:000", scoreKeeper.getScore());
     }
-
+/* 
     @Test
     public void testScoreTeamB3() {
         scoreKeeper.scoreTeamB3();
