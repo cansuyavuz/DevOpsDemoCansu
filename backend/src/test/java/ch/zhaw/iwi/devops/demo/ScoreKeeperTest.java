65ff876f3964ae326bb9f4fa22ccae02a2b520f4
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-11 14:07:54 +0200

    Testfall 5 inkl. Fehlermeldung

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ScoreKeeperTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ScoreKeeperTest.java
@@ -33,13 +33,13 @@ public class ScoreKeeperTest {
         scoreKeeper.scoreTeamA1();
         Assertions.assertEquals("001:000", scoreKeeper.getScore());
     }
- /* 
+
     @Test
     public void testScoreTeamB3() {
         scoreKeeper.scoreTeamB3();
-        Assertions.assertEquals("000:003", scoreKeeper.getScore());
+        Assertions.assertEquals("000:003XX", scoreKeeper.getScore());
     }
-
+ /* 
     @Test
     public void testScoreTeamB2() {
         scoreKeeper.scoreTeamB2();
