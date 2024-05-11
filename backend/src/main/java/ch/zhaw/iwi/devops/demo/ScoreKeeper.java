458663c3756419bf64af96cdfdf484e9d4d33154
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-11 13:25:40 +0200

    Testfall 1 Codeanpassung, Test bestanden

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/ScoreKeeper.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/ScoreKeeper.java
@@ -3,7 +3,11 @@ package ch.zhaw.iwi.devops.demo;
 public class ScoreKeeper {
     private int scoreTeamA = 0;
     private int scoreTeamB = 0;
-  /* 
+ 
+    public String getScore() {
+        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
+    }
+    /* 
     public void scoreTeamA3() {
         scoreTeamA += 3;
     }
@@ -28,9 +32,7 @@ public class ScoreKeeper {
     public void scoreTeamB1() {
         scoreTeamB += 1;
     }
-
-    public String getScore() {
-        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
-    }
 */
+  
+
 }
